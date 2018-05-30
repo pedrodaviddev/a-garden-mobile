11d1994f9da400cbb79f7051fd542eb66ac5c331
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2018-05-30 19:48:19 +0200

    Created image with circle bars on plant details

--- app/src/main/java/com/pedrodavidmcr/agarden/home/view/HomeFragment.kt
+++ app/src/main/java/com/pedrodavidmcr/agarden/home/view/HomeFragment.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pedrodavidmcr.agarden.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? =
       inflater.inflate(R.layout.fragment_home, container, false)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+
+  }
 }
